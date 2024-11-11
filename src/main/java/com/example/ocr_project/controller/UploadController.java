1386534cfbc3a91ba800fa0a1e9b4288ba0c717b
gilgyujeong <jgg7645@naver.com>
2024-11-12 01:42:29 +0900

    Feat: CORS append localhost:3000

--- src/main/java/com/example/ocr_project/controller/UploadController.java
+++ src/main/java/com/example/ocr_project/controller/UploadController.java
@@ -4,20 +4,18 @@ import com.example.ocr_project.dto.request.RequestImageUploadDto;
 import com.example.ocr_project.dto.response.ResponseStatusDto;
 import com.example.ocr_project.service.UploadService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class UploadController {
 
     private final UploadService uploadService;
 
     @PostMapping("/upload")
-    public ResponseStatusDto imageUpload(@RequestParam("image")MultipartFile multipartFile) {
+    public ResponseStatusDto imageUpload(@RequestParam("file")MultipartFile multipartFile) {
         return uploadService.imageUpload(multipartFile);
     }
 }
